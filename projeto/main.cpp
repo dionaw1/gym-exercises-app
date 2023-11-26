04e728ea48589123bac6333fa786075f8a551dc3
dogla <63586215+kouok123@users.noreply.github.com>
2023-11-26 12:17:06 -0300

    Add files via upload

--- projeto/main.cpp
+++ projeto/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <algorithm>
 #include <cstring>
+#include <stdlib.h>
 using namespace std;
 
 // Registro base que vai receber as informações do arquivo.
@@ -14,16 +15,7 @@ struct base
     int dificuldade;
 };
 
-struct base_char
-{
-    int id;
-    char nome[50];
-    char objetivo[20];
-    char musculos[30];
-    int dificuldade;
-};
-
-void remensionamento(base *&cadastro, int *&alteracoes, int tamanho, int &capacidade)
+void redimensionamento(base *&cadastro, int *&alteracoes, int tamanho, int &capacidade)
 {
     capacidade += 10;
     base *novoVetor = NULL;
@@ -38,27 +30,12 @@ void remensionamento(base *&cadastro, int *&alteracoes, int tamanho, int &capaci
     alteracoes = novoVetorModificado;
 }
 
-void converterVetor(base *vetorBase, base_char *vetorChar, size_t tamanho)
-{
-    for (size_t i = 0; i < tamanho; ++i)
-    {
-        vetorChar[i].id = vetorBase[i].id;
-
-        // Copiar string para o array de caracteres correspondente
-        strncpy(vetorChar[i].nome, vetorBase[i].nome.c_str(), sizeof(vetorChar[i].nome));
-        strncpy(vetorChar[i].objetivo, vetorBase[i].objetivo.c_str(), sizeof(vetorChar[i].objetivo));
-        strncpy(vetorChar[i].musculos, vetorBase[i].musculos.c_str(), sizeof(vetorChar[i].musculos));
-
-        vetorChar[i].dificuldade = vetorBase[i].dificuldade;
-    }
-}
-
 // Procedimento para inserir novos dados no arquivo.
-void inserir_CSV(base *&cadastro, int *&alteracoes, int &tamanho, int &capacidade)
+void inserir(base *&cadastro, int *&alteracoes, int &tamanho, int &capacidade)
 {
     // Verifica se há espaço suficiente e redimensiona se necessário.
     if (tamanho == (capacidade - 1))
-        remensionamento(cadastro, alteracoes, tamanho, capacidade);
+        redimensionamento(cadastro, alteracoes, tamanho, capacidade);
 
     cout << "Inserção de novos dados no arquivo.\n";
     cout << "Digite as informacoes do exercicio conforme solicitado.\n";
@@ -166,6 +143,33 @@ void alterarDado(base *cadastro, int *alteracoes, int i)
     } while (alteracoes[i] != 1 && valorAlterar != 0);
 }
 
+void deletar_exercicio(int *alteracoes, int i)
+{
+    cout << "Tem certeza que quer deletar esse exercicio, essa acao nao pode ser desfeita.\n";
+    int opcao_deletar = -1;
+    do
+    {
+        cout << "1 - Deletar mesmo assim.\n2 - Cancelar operacao.\n";
+        if (!(cin >> opcao_deletar))
+        {
+            cin.clear();
+            cin.ignore();
+        }
+        switch (opcao_deletar)
+        {
+        case 1:
+            alteracoes[i] = 2;
+            cout << "Exercicio deletado com sucesso. Salve o arquivo para aplicar as alteracoes\n";
+            break;
+        case 2:
+            break;
+        default:
+            cout << "Entrada invalida.\n";
+            break;
+        }
+    } while (opcao_deletar != 1 && opcao_deletar != 2);
+}
+
 // Função que lê um arquivo .csv e carrega os dados para um vetor de estruturas 'cadastro'. Retorna um booleano indicando se o carregamento do arquivo foi bem-sucedido ou não.
 bool ler_CSV(base *&cadastro, int *&alteracoes, int &tamanho, int &capacidade, string &cabecalho)
 {
@@ -176,7 +180,7 @@ bool ler_CSV(base *&cadastro, int *&alteracoes, int &tamanho, int &capacidade, s
         while (entrada >> cadastro[tamanho].id) // Usado para verificar se o fim do arquivo foi antingido, enquanto nao for o programa vai ler mais entradas.
         {
             if (tamanho == (capacidade - 1))
-                remensionamento(cadastro, alteracoes, tamanho, capacidade);
+                redimensionamento(cadastro, alteracoes, tamanho, capacidade);
 
             entrada.ignore();
             getline(entrada, cadastro[tamanho].nome, ';');
@@ -187,16 +191,10 @@ bool ler_CSV(base *&cadastro, int *&alteracoes, int &tamanho, int &capacidade, s
             tamanho++;
         }
         entrada.close();
-        cout << "Arquivo carregado com sucesso!" << endl;
-        cout << "Capacidade atual: " << capacidade << " Espaco usado: " << tamanho << endl;
         return true;
     }
     else
-
-    {
-        cout << "Erro ao carregar o arquivo.\n";
         return false;
-    }
 }
 
 // Procedimento que busca se o exercício que o usuário deseja consultar está presente no arquivo, tambem e responsavel por chamar outros procedimentos caso o usuario queira alterar ou deletar algum exercicio.
@@ -206,7 +204,7 @@ void buscaArquivo(base *cadastro, int *alteracoes, int tamanho)
     int itemAlterar = -1;
     int busca;
 
-    cout << "Buscar no arquivo.\nAtualmente o programa suporta dois tipos de busca:\n";
+    cout << "*\nBuscar no arquivo.\nAtualmente o programa suporta dois tipos de busca:\n";
 
     do
     {
@@ -227,10 +225,10 @@ void buscaArquivo(base *cadastro, int *alteracoes, int tamanho)
             int i = 0;
             while (i <= tamanho && !achou)
             {
-                if (cadastro[i].nome == exercicio)
+                if (cadastro[i].nome == exercicio && alteracoes[i] != 2)
                 {
-                    cout << "Exercicio encontrado, os seguintes dados estao cadastrados: \n"
-                         << endl;
+                    cout << "Exercicio encontrado, os seguintes dados estao cadastrados: \n";
+                    cout << endl;
                     cout << cadastro[i].id;
                     cout << " -- ";
                     cout << cadastro[i].nome;
@@ -246,6 +244,10 @@ void buscaArquivo(base *cadastro, int *alteracoes, int tamanho)
                     achou = true;
                 }
                 i++;
+            }
+            if (!achou)
+            {
+                cout << "Exercicio nao encontrado\n";
             }
         }
 
@@ -267,7 +269,7 @@ void buscaArquivo(base *cadastro, int *alteracoes, int tamanho)
                     int i = 0;
                     while (i <= tamanho && !achou)
                     {
-                        if (cadastro[i].id == idBusca)
+                        if (cadastro[i].id == idBusca && alteracoes[i] != 2)
                         {
                             cout << "Exercicio encontrado, os seguintes dados estao cadastrados: \n";
                             cout << "\nID: ";
@@ -287,6 +289,10 @@ void buscaArquivo(base *cadastro, int *alteracoes, int tamanho)
                         }
                         i++;
                     }
+                    if (!achou)
+                    {
+                        cout << "Exercicio nao encontrado\n";
+                    }
                 }
             } while (!achou);
         }
@@ -297,52 +303,64 @@ void buscaArquivo(base *cadastro, int *alteracoes, int tamanho)
 
     if (achou)
     {
-        cout << "\nDeseja alterar algo nesse exercicio?\n";
+        cout << "\n*\nQuer alterar algo neste exercicio?\n";
         int altera;
         do
         {
-            cout << "1 - Alterar.\n2 - Nao alterar.\n";
+            cout << "1 - Alterar.\n2 - Deletar exercicio.\n3 - Nao alterar.\n";
             cin >> altera;
-        } while (altera != 1 && altera != 2);
-        if (altera == 1)
+        } while (altera != 1 && altera != 2 && altera != 3);
+        switch (altera)
+        {
+        case 1:
             alterarDado(cadastro, alteracoes, itemAlterar);
+            break;
+        case 2:
+            deletar_exercicio(alteracoes, itemAlterar);
+            break;
+        case 3:
+            break;
+        default:
+            cout << "Entrada nao reconhecida.\n";
+            break;
+        }
     }
 
     else if (!achou && busca != 0)
-        cout << "Exercicio nao encontrado!\n" << endl;
+        cout << "Exercicio nao encontrado!\n"
+             << endl;
 }
 
 // Procedimento que salva as modificações do vetor 'cadastro', usado pra manipular os valores e salva as açterações no arquivo
-void escrever_CSV(base *cadastro, int *alteracoes, int tamanho, int capacidade, string cabecalho)
+void escrever_CSV(base *cadastro, int *alteracoes, int tamanho, string cabecalho)
 {
 
     bool foiModificado = false;
     int entrada;
     int i = 0;
 
-    while (i < capacidade && !foiModificado)
+    while (i < tamanho && !foiModificado)
     {
-        if (alteracoes[i] == 1)
+        if (alteracoes[i] == 1 || alteracoes[i] == 2)
             foiModificado = true;
         i++;
     }
-    if (!foiModificado)
-        cout << "Nenhuma alteração feita no arquivo.\n";
-    else
+
+    ofstream saida("dados.csv");
+    cout << "Deseja salvar todos os dados no arquivo ou apenas aqueles modificados?\n";
+
+    do
     {
-        ofstream saida("dados.csv");
-        cout << "Deseja salvar todos os dados no arquivo ou apenas aqueles modificados?\n";
+        cout << "1 - Todos os dados.\n2 - Apenas os modificados.\n";
+        cin >> entrada;
+        cin.ignore();
 
-        do
+        if (entrada == 1)
         {
-            cout << "1 - Todos os dados.\n2 - Apenas os modificados.\n";
-            cin >> entrada;
-            cin.ignore();
-
-            if (entrada == 1)
+            saida << cabecalho << '#' << endl;
+            for (int i = 0; i < tamanho; i++)
             {
-                saida << cabecalho << '#' << endl;
-                for (int i = 0; i < tamanho; i++)
+                if (alteracoes[i] != 2)
                 {
                     saida << cadastro[i].id;
                     saida << ";";
@@ -356,79 +374,131 @@ void escrever_CSV(base *cadastro, int *alteracoes, int tamanho, int capacidade, 
                     saida << endl;
                 }
             }
-            else if (entrada == 2)
+        }
+        else if (entrada == 2)
+        {
+            saida << cabecalho << '#' << endl;
+            for (int i = 0; i < tamanho; i++)
             {
-                saida << cabecalho << '#' << endl;
-                for (int i = 0; i < tamanho; i++)
+                if (alteracoes[i] == 1)
                 {
-                    if (alteracoes[i] == 1)
-                    {
-                        saida << cadastro[i].id;
-                        saida << ";";
-                        saida << cadastro[i].nome;
-                        saida << ";";
-                        saida << cadastro[i].objetivo;
-                        saida << ";";
-                        saida << cadastro[i].musculos;
-                        saida << ";";
-                        saida << cadastro[i].dificuldade;
-                        saida << endl;
-                    }
+                    saida << cadastro[i].id;
+                    saida << ";";
+                    saida << cadastro[i].nome;
+                    saida << ";";
+                    saida << cadastro[i].objetivo;
+                    saida << ";";
+                    saida << cadastro[i].musculos;
+                    saida << ";";
+                    saida << cadastro[i].dificuldade;
+                    saida << endl;
                 }
             }
-            else
-                cout << "Opção inválida, tente novamente." << endl;
+        }
+        else
+            cout << "Opção inválida, tente novamente." << endl;
 
-        } while (entrada != 1 && entrada != 2);
+    } while (entrada != 1 && entrada != 2);
 
-        cout << "Arquivo alterado com sucesso!" << endl;
+    cout << "Arquivo alterado com sucesso!" << endl;
 
-        saida.close();
-    }
+    saida.close();
 }
 
 // Função principal, usada pra chamar as demais funcoes e procedimentos do codigo, verificando se certas condições são atendidas.
 int main()
 {
     string cabecalho = "";
-    int tamanho = 0, capacidade = 75;
-    base *cadastro = NULL;
-    int *alteracoes = NULL;
+    int tamanho = 0, capacidade = 75; 
+
+    base *cadastro = NULL; //
     cadastro = new base[capacidade];
+
+    int *alteracoes = NULL; 
     alteracoes = new int[capacidade]();
 
-    if (ler_CSV(cadastro, alteracoes, tamanho, capacidade, cabecalho))
+    cout << "Qual o formato do arquivo a ser lido?\n";
+    int opcao_leitura = -1;
+    do
     {
-        int modo;
-        cout << "O que deseja fazer no arquivo?\n";
-        do
+        cout << "1 - Arquivo .csv.\n2 - Arquivo .bin.\n";
+        if (!(cin >> opcao_leitura))
         {
-            cout << "*\n1 - Buscar e alterar.\n2 - Inserir novos dados.\n3 - Salvar alteracoes no arquivo '.csv'. (Salve para efetivar as alteracoes no arquivo)\n0 - Sair do programa.\n";
-            if (!(cin >> modo))
+            cin.clear();
+            cin.ignore();
+        }
+        if (opcao_leitura == 1)
+        {
+            if (ler_CSV(cadastro, alteracoes, tamanho, capacidade, cabecalho))
+                cout << "Arquivo carregado com sucesso.\n";
+            else
             {
-                cin.clear();
-                cin.ignore();
+                cout << "Erro ao tentar carregar o arquivo.\n";
+                return 1;
             }
-            switch (modo)
+        }
+        else if (opcao_leitura == 2)
+        {
+            //Opcao para ler o de um arquivo binario...
+        }
+        else
+            cout << "Opcao invalida.\n";
+    } while (opcao_leitura != 1 && opcao_leitura != 2);
+
+    int modo;
+    cout << "O que deseja fazer no arquivo?\n";
+    do
+    {
+        cout << "*\n1 - Consultar (alterar e deletar).\n2 - Inserir novos dados.\n3 - Salvar alteracoes no arquivo '.csv'. (Salve para efetivar as alteracoes no arquivo)\n0 - Sair do programa.\n";
+        if (!(cin >> modo))
+        {
+            cin.clear();
+            cin.ignore();
+            system("cls");
+            modo = -1;
+        }
+        if (modo == 1)
+        {
+            buscaArquivo(cadastro, alteracoes, tamanho);
+        }
+        else if (modo == 2)
+        {
+            inserir(cadastro, alteracoes, tamanho, capacidade);
+        }
+        else if (modo == 3)
+        {
+            cout << "Como deseja salvar o arquivo?\n";
+            int opcao_salvar = -1;
+            do
             {
-            case 1:
-                buscaArquivo(cadastro, alteracoes, tamanho);
-                break;
-            case 2:
-                inserir_CSV(cadastro, alteracoes, tamanho, capacidade);
-                break;
-            case 3:
-                escrever_CSV(cadastro, alteracoes, tamanho, capacidade, cabecalho);
-                break;
-            case 0:
-                cout << "Programa encerrado.\n";
-                break;
-            default:
-                cout << "Entrada nao reconhecida.\n";
-                break;
-            }
-        } while (modo != 0);
-    }
+                cout << "1 - Salvar como .csv.\n2 - Salvar como .bin\n";
+                if (!(cin >> opcao_salvar))
+                {
+                    cin.clear();
+                    cin.ignore();
+                }
+                if (opcao_salvar == 1)
+                {
+                    escrever_CSV(cadastro, alteracoes, tamanho, cabecalho);
+                }
+                else if (opcao_salvar == 2)
+                {
+                    //Opcao para salvar o arquivo em binario...
+                }
+                else
+                    cout << "Opcao invalida\n";
+            } while (opcao_salvar != 1 && opcao_salvar != 2);
+        }
+        else if (modo == 0)
+        {
+            cout << "Programa encerrado.\n";
+            return 0;
+        }
+        else
+        {
+            cout << "Entrada nao reconhecida.\n";
+        }
+    } while (modo != 0);
 
     delete[] cadastro;
     delete[] alteracoes;
